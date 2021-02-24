229b0a1350922dec792fd60185910741e0e24885
Michael Edelman <metalman@amazon.com>
2021-02-24 18:45:03 -0500

    f npm

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import { getAllPostsForHome } from "../lib/api";
+import { useAmp } from "next/amp";
+
+export const config = { amp: true };
 
 export default function Home({ allPosts, preview }) {
+  const isAmp = useAmp();
   return <div>Hello</div>;
 }
 
