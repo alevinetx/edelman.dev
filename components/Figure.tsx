9b1bf4653ef89715ebd903cc9ffded32cb249deb
Michael Edelman <metalman@amazon.com>
2021-03-01 17:09:50 -0500

    component updates

--- components/Figure.tsx
+++ components/Figure.tsx
@@ -7,13 +7,22 @@ import imageUrlBuilder from "@sanity/image-url";
 const builder = imageUrlBuilder(sanity);
 
 function urlFor(source) {
+  const t = builder.image(source).url();
+  // console.log(t);
   return builder.image(source);
 }
 
+const myLoader = (props) => {
+  // console.log("loader props", props);
+  return `${props.src}&w=${props.width}`;
+};
+
 export function Figure(props) {
+  urlFor(props.node);
+  const { loader, ...rest } = useNextSanityImage(sanity, props.node);
+  // console.log(props);
+
   return (
-    <img
-      src={urlFor(props.node).fit("crop").crop("entropy").width(800).url()}
-    />
+    <Img {...rest} loader={myLoader} sizes="(max-width: 800px) 100vw, 800px" />
   );
 }
