64128f65643dd86d2933ebcca8f43f999ce2cd56
Michael Edelman <metalman@amazon.com>
2021-03-01 20:28:46 -0500

    custom img builder

--- components/Figure.tsx
+++ components/Figure.tsx
@@ -6,18 +6,31 @@ import imageUrlBuilder from "@sanity/image-url";
 
 const builder = imageUrlBuilder(sanity);
 
+const myCustomImageBuilder = (imageUrlBuilder, options) => {
+  return imageUrlBuilder
+    .width(
+      options?.width !== null
+        ? options.width
+        : Math.min(options.originalImageDimensions.width, 800)
+    )
+    .fit("clip")
+    .auto("format");
+};
+
 function urlFor(source) {
-  const t = builder.image(source).url();
-  // console.log(t);
+  const t = builder.image(source).auto("format").url();
+
   return builder.image(source);
 }
 
 const myLoader = (props) => {
-  return `${props.src}&w=${props.width}&q=50`;
+  return `${props.src}&w=${props.width}`;
 };
 
 export function Figure(props) {
-  const { loader, ...rest } = useNextSanityImage(sanity, props.node);
+  const { loader, ...rest } = useNextSanityImage(sanity, props.node, {
+    imageBuilder: myCustomImageBuilder,
+  });
 
   return (
     <Img
@@ -25,6 +38,7 @@ export function Figure(props) {
       loader={myLoader}
       sizes="(max-width: 800px) 100vw, 800px"
       alt={props.node.alt}
+      priority={true}
     />
   );
 }
